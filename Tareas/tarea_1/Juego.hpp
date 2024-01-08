462b5df8a51fdca2724417b45e234bd4e5216595
emmanuelavilesucr <emmanuel.aviles@ucr.ac.cr>
2024-01-08 15:30:19 -0600

    Correccion de la sintaxis de la clase Juego
    Signed-off-by: emmanuelavilesucr <emmanuel.aviles@ucr.ac.cr>

--- Tareas/tarea_1/Juego.hpp
+++ Tareas/tarea_1/Juego.hpp
@@ -1,17 +1,17 @@
 #ifndef JUEGO_HPP
 #define JUEGO_HPP
 
-class Juego() {
+class Juego {
 public:
     Juego();
     void mostrarMenu();
     void iniciarJuegoFacil();
     void iniciarJuegoDificil();
-    
+
 private:
     void jugar(bool modoDificil);
     void imprimirPista(int numeroSecreto, int intentoUsuario);
 
 };
 
-#endif
+#endif  
