a702c9eab23b8575a8c0262c9f318bf07361cde9
emmanuelavilesucr <emmanuel.aviles@ucr.ac.cr>
2024-01-08 15:25:43 -0600

    Agrego logica de los metodos jugar y imprimirPista, encargados de la funcionalidad y mecanicas del juego
    Signed-off-by: emmanuelavilesucr <emmanuel.aviles@ucr.ac.cr>

--- Tareas/tarea_1/Juego.cpp
+++ Tareas/tarea_1/Juego.cpp
@@ -40,3 +40,39 @@ void Juego::iniciarJuegoFacil(){
 void Juego::iniciarJuegoDificil(){
     jugar(true);
 } 
+
+void Juego::jugar(bool modoDificil){
+    const int limite = modoDificil ? 100: 50; 
+    const int numeroSecreto = std::rand() % limite + 1;
+    const int intentos = limite / 3;
+
+    std::cout << "¡Bienvenido!\n";
+    std::cout << "Estoy pensando en un número entre 1 y : ??? " << limite << ".\n";
+    std::cout << "Tienes " << intentos << " intentos para adivinar.\n";
+
+    for (int intento = 1; intento <= intentos; ++intento){
+        int intentoUsuario;
+        std::cout << "Intento #" << intento << ": ";
+        std::cin << intentoUsuario; 
+
+        if (intentoUsuario == numeroSecreto) {
+            std::cout << "¡Felicidades! Has adivinado el número.\n";
+            return;
+        } else {
+            imprimirPista(numeroSecreto, intentoUsuario);
+        }
+    }
+    std::cout << "Lo siento, has agotado tus intentos. El número correcto era: " << numeroSecreto << "\n";
+
+}
+
+void Juego::imprimirPista(int numerosecreto, int intentoUsuario){
+
+    if (intentoUsuario < numeroSecreto) {
+        std::cout << "El número es mayor. ";
+    } else {
+        std::cout << "El número es menor. ";
+    }
+    std::cout << "¡Sigue intentándolo!\n"; // Se pueden modificar las frases con: "congelado", "frío", "caliente" o "hirviendo".
+
+}
