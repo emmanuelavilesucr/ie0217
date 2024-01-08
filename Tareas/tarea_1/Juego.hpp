c184a73ba217ee2a4875ca115b7f5fae8d98bf72
emmanuelavilesucr <emmanuel.aviles@ucr.ac.cr>
2024-01-08 15:00:42 -0600

    Agrego los metodos jugar y imprimirPista, encargados de gestionar las mecanicas del juego. Encapsulo su funcionalidad por medio de private
    Signed-off-by: emmanuelavilesucr <emmanuel.aviles@ucr.ac.cr>

--- Tareas/tarea_1/Juego.hpp
+++ Tareas/tarea_1/Juego.hpp
@@ -2,10 +2,15 @@
 #define JUEGO_HPP
 
 class Juego() {
+public:
     Juego();
     void mostrarMenu();
     void iniciarJuegoFacil();
     void iniciarJuegoDificil();
+    
+private:
+    void jugar(bool modoDificil);
+    void imprimirPista(int numeroSecreto, int intentoUsuario);
 
 };
 
