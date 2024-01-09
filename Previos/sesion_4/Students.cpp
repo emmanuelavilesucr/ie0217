6f49f8c06e80f64c827535ab6f620c4203e2b93a
emmanuelavilesucr <emmanuel.aviles@ucr.ac.cr>
2024-01-08 20:14:11 -0600

    Creacion de la estructura del codigo Students.cpp y de sus instancias
    Signed-off-by: emmanuelavilesucr <emmanuel.aviles@ucr.ac.cr>

---
+++ Previos/sesion_4/Students.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+class Student {
+
+
+};
+
+void calcularAverage(Student s1, Student s2){
+
+
+}
+
+
+int main(){
+
+    Student student1(88.0), student(56.0);
+    calculateAverage(Student1, s=Student2);
+    return 0;
+}
