245d3f3efca6a4c00a214eb55b6fbddfaa63e28c
emmanuelavilesucr <emmanuel.aviles@ucr.ac.cr>
2024-01-08 20:21:21 -0600

    Correcion en la logica del main en el ejercicio Students.cpp
    Signed-off-by: emmanuelavilesucr <emmanuel.aviles@ucr.ac.cr>

--- Previos/sesion_4/Students.cpp
+++ Previos/sesion_4/Students.cpp
@@ -2,19 +2,22 @@
 using namespace std;
 
 class Student {
-
-
+    public:
+        double marks;
+        Student(double m) {
+            marks = m ;
+        }
 };
 
-void calcularAverage(Student s1, Student s2){
-
-
+void calculateAverage(Student s1, Student s2){
+    double average = (s1.marks + s2.marks) /2;
+    cout << "Average Marks = " << average << endl;
 }
 
 
 int main(){
 
-    Student student1(88.0), student(56.0);
-    calculateAverage(Student1, s=Student2);
+    Student student1(88.0), student2(56.0);
+    calculateAverage(student1, student2);
     return 0;
 }
