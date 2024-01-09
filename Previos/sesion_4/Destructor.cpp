9aa3e5106b90e92c6d1883358da7460f9221b0ca
emmanuelavilesucr <emmanuel.aviles@ucr.ac.cr>
2024-01-08 18:51:47 -0600

    Agrege el destructor y la logica de la clase y la funcion main del ejercicio Destructor.cpp
    Signed-off-by: emmanuelavilesucr <emmanuel.aviles@ucr.ac.cr>

--- Previos/sesion_4/Destructor.cpp
+++ Previos/sesion_4/Destructor.cpp
@@ -2,15 +2,26 @@
 #include <fstream>
 
 class Archivo {
+    private:
+        std::fstream archivo;
 
-
-
+    public:
+        Archivo(std::string nombre_archivo) {
+            archivo.open(nombre_archivo, std::ios::in | std::ios | std::ios::out | std::ios::app);
+            if (!archivo.is_open()) {
+                std::cout << "No es pudo abrir el archivo " << nombre_archivo << std::endl;
+            }
+        }
+        
+        ~Archivo(){      // Destructor 
+            if (!archivo.is_open()){
+                archivo.close();
+            }
+        }
 };
 
 
 int main(){
-
-
-
+    Archivo mi_archivo("datos.txt");
     return 0;
 } 
