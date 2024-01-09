a99c21028f2b0b5a4108bd6f64659f1feb096456
emmanuelavilesucr <emmanuel.aviles@ucr.ac.cr>
2024-01-08 19:28:59 -0600

    Agregar ejercicio Animal.cpp
    Signed-off-by: emmanuelavilesucr <emmanuel.aviles@ucr.ac.cr>

---
+++ Previos/sesion_4/Animal.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+class Animal {
+    public:
+        void setEdad(int age){
+            edad = age;
+        }
+
+        int getEdad(){
+            return edad;
+        }
+
+    protected:
+        string nombre;
+
+    private:
+        int edad;
+};
+
+class Perro : public Animal {
+    public:
+        void setNombre(string n){
+            return nombre;
+        }
+        string getNombre(){
+            return nombre;
+        }
+};
+
+int main(){
+    Perro miPerro;
+    miPerro.setNombre("Toby");
+    miPerro.setEdad(3);
+
+    cout << "Mi perro se llama " << miPerro.getNombre() << endl;
+    cout << "y tiene " << miPerro.getEdad() << " anios. " << endl;
+
+    return 0;
+}
+
