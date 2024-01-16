697757e08a4ac0570cb4bbf8faaed1ecab73cc2c
emmanuelavilesucr <emmanuel.aviles@ucr.ac.cr>
2024-01-15 20:14:17 -0600

    Agregar sesion 9

---
+++ Previos/Previos_3/sesion_9/biblioteca/biblioteca.hpp
@@ -0,0 +1,18 @@
+#ifndef BIBLIOTECA_HPP
+#define BIBLIOTECA_HPP
+
+#include <vector>
+#include "libro.cpp"
+
+class Biblioteca {
+    public:
+        void agregarLibro(
+            const std::string& titulo,
+            const std::string& autor
+        );
+        void mostrarCatalogo();
+    private:
+        std::vector<Libro> catalogo;
+};
+
+#endif
---
+++ Previos/Previos_3/sesion_9/biblioteca/main.cpp
@@ -0,0 +1,13 @@
+#include "biblioteca.hpp"
+
+int main() {
+
+    Biblioteca biblioteca;
+
+    biblioteca.agregarLibro("El Gran Gatsby", "F. Scott Fitzgerald");
+    biblioteca.agregarLibro("1984", "George Orwell");
+
+    biblioteca.mostrarCatalogo();
+
+    return 0;
+}
