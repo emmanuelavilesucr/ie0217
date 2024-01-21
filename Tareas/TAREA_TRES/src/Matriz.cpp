57e4c065e88fb31d3066cb3d4006ff65aac99530
emmanuelavilesucr <emmanuel.aviles@ucr.ac.cr>
2024-01-21 14:05:15 -0600

    Agregar logica de la definicion  del codigo Matriz

--- Tareas/TAREA_TRES/src/Matriz.cpp
+++ Tareas/TAREA_TRES/src/Matriz.cpp
@@ -1,4 +1,35 @@
 #include "Matriz.hpp"
 #include <iostream>
 
+template <typename T>
+Matriz<T>::Matriz() : filas(0), columnas(0) {}
 
+template <typename T>
+Matriz<T>::Matriz(int filas, int columnas) : filas(filas), columnas(columnas) {
+    datos.resize(filas, std::vector<T>(columnas));
+}
+
+template <typename T>
+void Matriz<T>::pedirDatos() {
+
+}
+
+template <typename T>
+void Matriz<T>::imprimirMatriz() const {
+
+}
+
+template <typename T>
+Matriz<T> Matriz<T>::operator+(const Matriz<T>& otra) const {
+
+}
+
+template <typename T>
+Matriz<T> Matriz<T>::operator-(const Matriz<T>& otra) const {
+
+}
+
+template <typename T>
+Matriz<T> Matriz<T>::operator*(const Matriz<T>& otra) const {
+
+}
