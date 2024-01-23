26cbc0521de0018f4679e72410c82faef81cf0cc
emmanuelavilesucr <emmanuel.aviles@ucr.ac.cr>
2024-01-22 20:28:54 -0600

    Agregar logica de la definicion

--- Tareas/TAREA_TRES/src/ejercicio_2/validadorCorreo.cpp
+++ Tareas/TAREA_TRES/src/ejercicio_2/validadorCorreo.cpp
@@ -2,8 +2,24 @@
 #include <iostream>
 #include <regex>
 
-bool ValidadorCorreoElectronico::validarCorreo(const std::string& correo) {
 
+bool ValidadorCorreoElectronico::validarCorreo(const std::string& correo) {
+        try {
+        if (!tieneArroba(correo)) {
+            throw std::invalid_argument("La direccion de correo electronico debe contener un '@'.");
+        }
+
+        std::regex formatoCorreo("^[a-zA-Z0-9.-]{1,15}@[a-zA-Z]+\\.[a-zA-Z]{2,4}$");
+
+        if (!std::regex_match(correo, formatoCorreo)) {
+            throw std::invalid_argument("La direccion de correo electronico no sigue el formato estandar.");
+        }
+
+        return true;
+    } catch (const std::invalid_argument& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+        return false;
+    }
 }
 
 bool ValidadorCorreoElectronico::tieneArroba(const std::string& correo) {
