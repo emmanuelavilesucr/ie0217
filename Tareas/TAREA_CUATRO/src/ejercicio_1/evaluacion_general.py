96362029fb7891b326a2614e593295d83be52a0d
Emmanuel <emmanuel_aviles@Ubuntu.myguest.virtualbox.org>
2024-01-28 12:57:23 -0600

    Agregar logica del modulo evaluacion_general.py

--- Tareas/TAREA_CUATRO/src/ejercicio_1/evaluacion_general.py
+++ Tareas/TAREA_CUATRO/src/ejercicio_1/evaluacion_general.py
@@ -1,12 +1,27 @@
 class EvaluacionGeneral:
-    def __init__(self):
-        
+    def __init__(self, evaluacion_especifica):
+        self.evaluacion_especifica = evaluacion_especifica
+        self.alergias_evaluadas = []
 
     def calcular_puntuacion_general(self):
-        
-
-
+        puntuacion = sum(alergia.valor for alergia in self.alergias_evaluadas)
+        return puntuacion
 
     def imprimir_resultados(self):
+                puntuacion_general = self.calcular_puntuacion_general()
+        print(f"Puntuación general de alergias: {puntuacion_general}")
+
+        if self.evaluacion_especifica.nombres_desconocidos:
+            print("Resultados Desconocidos (nombres):", ', '.join(self.evaluacion_especifica.nombres_desconocidos))
+
+        if self.evaluacion_especifica.valores_desconocidos:
+            print("Resultados Desconocidos (valores):", ', '.join(map(str, self.evaluacion_especifica.valores_desconocidos)))
+
+        if self.alergias_evaluadas:
+            promedio = sum(alergia.valor for alergia in self.alergias_evaluadas) / len(self.alergias_evaluadas)
+            print(f"Promedio de alergias conocidas: {promedio}")
+        else:
+            print("No se encontraron alergias conocidas.")
+
     
 
