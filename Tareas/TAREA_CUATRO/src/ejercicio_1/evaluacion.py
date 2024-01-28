385dfff6f8ec98c67383ac62b8f351b95be8add0
Emmanuel <emmanuel_aviles@Ubuntu.myguest.virtualbox.org>
2024-01-28 12:42:13 -0600

    Agregar logica del modulo evaluacion.py

--- Tareas/TAREA_CUATRO/src/ejercicio_1/evaluacion.py
+++ Tareas/TAREA_CUATRO/src/ejercicio_1/evaluacion.py
@@ -1,10 +1,23 @@
+from alergia import Alergia
+
 class EvaluacionEspecifica:
     def __int__(self):
+        self.alergias = []
+        self.nombres_desconocidos = []
+        self.valores_desconocidos = []
 
+    def evaluar_alergias(self, puntuacion):
+        alergias_activas = []
+        for alergia in self.alergias:
+            if puntuacion & alergia.valor:
+                alergias_activas.append(alergia)
+        return alergias_activas
 
+    def imprimir_evaluacion(self, puntuacion):
+        alergias_activas = self.evaluar_alergias(puntuacion)
+        print(f"Puntuación general: {puntuacion}")
+        print("Alergias activas:")
+        for alergia in alergias_activas:
+            print(f"- {alergia}")
+        print()
 
-    def evaluar_alergias(self):
-
-
-
-    def imprimir_evaluacion(self):
