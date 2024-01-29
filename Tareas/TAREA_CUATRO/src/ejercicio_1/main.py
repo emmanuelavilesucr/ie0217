2f58dcde1140e5a7d19830d3f0599b6f7004c7d2
Emmanuel <emmanuel_aviles@Ubuntu.myguest.virtualbox.org>
2024-01-29 12:19:29 -0600

    Corregir sintaxis

--- Tareas/TAREA_CUATRO/src/ejercicio_1/main.py
+++ Tareas/TAREA_CUATRO/src/ejercicio_1/main.py
@@ -24,15 +24,15 @@ def interfaz_usuario():
             evaluacion_especifica.imprimir_evaluacion(puntuacion_general)
 
         elif opcion == "2":
-            entrada_usuario = input("Ingrese el nombre o valor de la alergia (o 'Salir' para terminar): ")
-            while entrada_usuario.lower() != 'Salir':
+            entrada_usuario = input("Ingrese el nombre o valor de la alergia (o 'fin' para terminar): ")
+            while entrada_usuario.lower() != 'fin':
                 alergia = tipos_alergias.analizar_entrada_usuario(entrada_usuario)
                 if alergia:
                     evaluacion_especifica.alergias.append(alergia)
                     evaluacion_general.alergias_evaluadas.append(alergia)
                 else:
                     print("Entrada no valida. Intente nuevamente.")
-                entrada_usuario = input("Ingrese el nombre o valor de la alergia (o 'Salir' para terminar): ")
+                entrada_usuario = input("Ingrese el nombre o valor de la alergia (o 'fin' para terminar): ")
 
         elif opcion == "3":
             print("Saliendo del programa.")
