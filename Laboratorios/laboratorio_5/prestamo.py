abf88101f3c7ac76c71a3ab70f1a3e3b273aa9c6
emmanuelavilesucr <emmanuel.aviles@ucr.ac.cr>
2024-02-01 19:15:38 -0600

    Agregar comentarios extras

--- Laboratorios/laboratorio_5/prestamo.py
+++ Laboratorios/laboratorio_5/prestamo.py
@@ -1,29 +1,34 @@
+# Se importan las librerias a utilizar 
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
 class Prestamo:
+    
+    # Esta funcion es la encargada de la inicializacion de los atributos de la clase
     def __init__(self, monto_prestamo, tasa_interes_anual, cuotas):
         self.monto_prestamo = monto_prestamo
         self.tasa_interes_anual = tasa_interes_anual
         self.cuotas = cuotas
         self.amortizacion = self.calcular_amortizacion()
         
-              
+    # Funcion encargada del calculo de la amortizacion de los prestamos          
     def calcular_amortizacion(self):
         try:
-            tasa_interes_mensual = self.tasa_interes_anual / 12 / 100
+            tasa_interes_mensual = self.tasa_interes_anual / 12 / 100        # Se establece la formula de la tasa de interes
             cuota_mensual = (tasa_interes_mensual * self.monto_prestamo) / (1 - (1 + tasa_interes_mensual)** -self.cuotas)
             
             saldo_restante = self.monto_prestamo
             amortizacion = []
             
-            
+            # Mediante el siguiente ciclo for se calcula la amortizacion para cada cuota
             for cuota in range(1, self.cuotas + 1):
                 interes_pendiente = saldo_restante * tasa_interes_mensual
                 amortizacion_principal = cuota_mensual - interes_pendiente
                 
                 saldo_restante -= amortizacion_principal
+                
+                # A continuacion se almacena la informacion en la lista
                 amortizacion.append({"Cuota": cuota, "Interes": interes_pendiente, "Amortizacion": amortizacion_principal, "Saldo Restante": saldo_restante})
                 
             return amortizacion
@@ -32,31 +37,35 @@ class Prestamo:
             print("Error: La cantidad de cuotas no puede ser cero.")
             return []
         
-        
+   # Funcion encargada de generar el reporte del prestamo     
     def generar_reporte(self, archivo_salida):
         try:
-            df = pd.DataFrame(self.amortizacion)
-            df.to_csv(archivo_salida, index=False)
+            df = pd.DataFrame(self.amortizacion)    # Crea un DataFrame con la informacion de la amortizacion
+            df.to_csv(archivo_salida, index=False)   # Se guardan los datos de DataFrame en un archivo CSV
             print(f"Reporte generado con exito: {archivo_salida}")
             
         except Exception as e:
             print(f"Ocurrio un error al generar el reporte: {str(e)}")
             
-            
+    # Esta funcion crea la grafica del prestamo utilizando pandas
     def graficar_amortizacion(self):
-        df = pd.DataFrame(self.amortizacion)
         
-        data = np.array([df["Interes"], df["Amortizacion"]])
+        df = pd.DataFrame(self.amortizacion)  # Se crea un DataFrame de los datos del interes y amortizacion
+    
+        data = np.array([df["Interes"], df["Amortizacion"]])  
         
+        # Crea un grafico de barras
         plt.bar(df["Cuota"], data[0], label="Interes")
         plt.bar(df["Cuota"], data[1], bottom = data[0], label = "Amortizacion")
         
+        # Se realiza la configuracion de grafico
         plt.xlabel("Numero de cuota")
         plt.ylabel("Monto ($)")
         plt.title("Amotizacion e Interes por Cuota")
         plt.legend()
         plt.show()
-        
+
+# Se declara la funcion del main
 def main():
     try:
         # 1. Pedir el monto al usuario
@@ -86,9 +95,9 @@ def main():
     except Exception as e:
         print(f"Error: {str(e)}")
 
+
 if __name__ == "__main__":
-    main()
- 
+    main()   # Llama a la funcion main 
         
         
    
