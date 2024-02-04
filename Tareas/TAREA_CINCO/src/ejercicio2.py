2d4cc1cfe2913d4524f65c20ae2e10b5955194bb
emmanuelavilesucr <emmanuel.aviles@ucr.ac.cr>
2024-02-04 14:37:31 -0600

    Agregar estrutuctura base del codigo del ejercicio 2

---
+++ Tareas/TAREA_CINCO/src/ejercicio2.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+
+# Declaracion de variables globales 
+
+def promedio():
+    
+    # Codigo encargado del calculo del promedio de lso estudiantes
+    # Mediantes funciones de la biblioteca numpy 
+    
+    return 
+    
+    
+
+    
+def main():
+    
+    
+    
+    
+    return 
+main()
+    
+    
