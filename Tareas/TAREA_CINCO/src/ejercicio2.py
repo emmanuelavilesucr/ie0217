c11cc86e063d15b35effee51b983e7f2449f277a
emmanuelavilesucr <emmanuel.aviles@ucr.ac.cr>
2024-02-04 14:46:34 -0600

    Declaracion de la variables globales

--- Tareas/TAREA_CINCO/src/ejercicio2.py
+++ Tareas/TAREA_CINCO/src/ejercicio2.py
@@ -1,9 +1,13 @@
 import numpy as np
 
+np.random.seed(42)  # Voy a utilizar esta linea para que el programa sea reproductile y no aleatorio
 
-# Declaracion de variables globales 
+# Declaracion de la variables globales del codigo 
+materias = 5
+alumnos =  5
+datos = np.random.randint(0, 101, size=(alumnos, materias))
 
-def promedio():
+def promedio(datos):
     
     # Codigo encargado del calculo del promedio de lso estudiantes
     # Mediantes funciones de la biblioteca numpy 
