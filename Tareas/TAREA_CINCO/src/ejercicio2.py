2e31d727526df73aff01c4258ed8bf29d0246a40
emmanuelavilesucr <emmanuel.aviles@ucr.ac.cr>
2024-02-04 15:14:38 -0600

    Agregar funcion promedio

--- Tareas/TAREA_CINCO/src/ejercicio2.py
+++ Tareas/TAREA_CINCO/src/ejercicio2.py
@@ -7,16 +7,17 @@ materias = 5
 alumnos =  5
 datos = np.random.randint(0, 101, size=(alumnos, materias))
 
+# Funcion encargada del calculo de los promedios de los estudiantes mediante uso de la biblioteca NumPy.
 def promedio(datos):
     
-    # Codigo encargado del calculo del promedio de lso estudiantes
-    # Mediantes funciones de la biblioteca numpy 
+    promedio_alumno = np.mean(datos, axis=1)
+    promedio_materias = np.mean(datos, axis=0) 
+    maximo = np.max(datos, axis=1)
+    suma = np.sum(datos, axis=0)
     
     return 
     
-    
-
-    
+# Funcion del main 
 def main():
     
     
