db36d83af4452ba57b685b433b26c72f3ecc10e3
emmanuelavilesucr <emmanuel.aviles@ucr.ac.cr>
2024-02-07 10:16:35 -0600

    Agregar documentacion. Ademas se agregaron dos graficos encargados de mostrar los promedios del alumno y por asignatura

--- Tareas/TAREA_CINCO/src/ejercicio2.py
+++ Tareas/TAREA_CINCO/src/ejercicio2.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 np.random.seed(42)  # Voy a utilizar esta linea para que el programa sea reproductile y no aleatorio
 
@@ -9,6 +10,13 @@ datos = np.random.randint(0, 101, size=(alumnos, materias))
 
 # Funcion encargada del calculo de los promedios de los estudiantes mediante uso de la biblioteca NumPy.
 def promedio(datos):
+    """
+    Funcion encargada de los calculos.
+
+    @return:
+        Tuple: Una tupla que contiene los datos.
+    """
+    
     
     promedio_alumno = np.mean(datos, axis=1)
     promedio_materias = np.mean(datos, axis=0) 
@@ -20,15 +28,37 @@ def promedio(datos):
 # Funcion del main 
 def main():
     
+    """
+    Funcion main encargada de muestrar los datos y los grafico de promedios.
+    """
+    
     promedio_alumno, promedio_materias, maximo, suma = promedio(datos) 
 
-   
     print("Conjunto de Datos:\n", datos)
     print("\nResultado:")
     print("Promedio del alumno:", promedio_alumno)
     print("Promedio de la materia:", promedio_materias)
     print("Calificación mas alta:", maximo)
     print("Suma de calificaciones:", suma)
+
+    # Cofiguracion de los graficos de promedios
+    plt.figure(figsize=(10, 5))
+    plt.bar(np.arange(1, alumnos+1), promedio_alumno, color='skyblue')
+    plt.title('Promedio de calificaciones por alumno')
+    plt.xlabel('Alumno')
+    plt.ylabel('Promedio de calificaciones')
+    plt.xticks(np.arange(alumnos))
+    plt.grid(axis='y', linestyle='--', alpha=0.7)
+    plt.show()
+    
+    plt.figure(figsize=(10, 5))
+    plt.bar(np.arange(1, materias+1), promedio_materias, color='lightgreen')
+    plt.title('Promedio de calificaciones por materia')
+    plt.xlabel('Materia')
+    plt.ylabel('Promedio de calificaciones')
+    plt.xticks(np.arange(materias))
+    plt.grid(axis='y', linestyle='--', alpha=0.7)
+    plt.show()
     
     
 main()   
