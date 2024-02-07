f05d9c29d8633594c55d280154acb2c38cd06769
emmanuelavilesucr <emmanuel.aviles@ucr.ac.cr>
2024-02-06 19:40:18 -0600

    Agregar codigo de la funcion visualizar. En este archivo tambien se corrigeron los nombres de las clases y se crearon nuevos atributos

--- Tareas/TAREA_CINCO/src/grafico.py
+++ Tareas/TAREA_CINCO/src/grafico.py
@@ -1,17 +1,40 @@
-from datos import BTS 
-
+import matplotlib.pyplot as plt
+import seaborn as sns
+import matplotlib.ticker as ticker
+from datos import Datos
 
 class Grafico:
     def __init__(self):
-        self.datos = BTS  
+        self.datos = Datos()  
+        self.df = self.datos.obtener_datos()
         
-    def visualizar():
+    def visualizar(self):
+       
+        df = self.datos.obtener_datos()
+        df = self.datos.limpieza_datos()
+        descripcion, contar_vuelos, detalles_pasajeros = self.datos.analisis_datos()
         
-        # El codigo de este metodo debe ser capaz generar 2 graficos de los datos analizados.(Mediante Matplotlib) 
-        # Los graficos deben ser de diferentes tipos.
-        # Resaltar informacion relevante. (mediante Seaborn).
-        # Usar generador para los datos de visualizacion.
+        plt.figure(figsize=(8, 6))
+        sns.barplot(x="Año", y="Llegadas puntuales", data=df)
+        plt.title("Llegadas puntuales")
+        plt.xlabel("Año")
+        plt.ylabel("Llegadas puntuales")
+        plt.xticks(rotation=45)
+        plt.gca().get_yaxis().set_major_formatter(ticker.FuncFormatter(lambda x, _: "{:,}".format(int(x))))
+        plt.tight_layout()
+        plt.show()
+        
+
+        plt.figure(figsize=(8, 6))
+        sns.lineplot(x="Año", y="Retrasos en la llegada", data=df, marker="o")
+        plt.title("Retrasos en la llegada")
+        plt.xlabel("Año")
+        plt.ylabel("Retrasos en la llegada")
+        plt.xticks(rotation=45)
+        plt.gca().get_yaxis().set_major_formatter(ticker.FuncFormatter(lambda x, _: "{:,}".format(int(x))))
+        plt.tight_layout()
+        plt.show()
+
         return
     
-   
     
