b7acc861d1034adf6be25d208f3d00c54fff0ed1
emmanuelavilesucr <emmanuel.aviles@ucr.ac.cr>
2024-02-06 19:44:28 -0600

    Agregar funcion de main

--- Tareas/TAREA_CINCO/src/main.py
+++ Tareas/TAREA_CINCO/src/main.py
@@ -1,10 +1,27 @@
-from grafico import Grafico 
+from grafico import Grafico
+import time
 
 def main():
-    # Codigo del main 
-    # Llama a los otros metodos de la clases
     
-      
-    return
+    grafico = Grafico()
+    
+    while True:
+        print("\n-------------Menu------------")
+        print("1. Visualizar graficos")
+        print("2. Ver tabla de datos")
+        print("3. Salir")
+
+        opcion = input("Ingresa una opcion: ")
+
+        if opcion == "1":
+            grafico.visualizar()
+        elif opcion == "2":
+            grafico.mostrar_tabla()
+            time.sleep(5)
+        elif opcion == "3":
+            print("------Cerrando programa------")
+            break  
+        else:
+            print("Opcion invaelida")
 
 main()
