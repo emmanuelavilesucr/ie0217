afdbdd477ac7db785857e5790badf5100e762085
emmanuelavilesucr <emmanuel.aviles@ucr.ac.cr>
2024-02-06 19:20:26 -0600

    Agregar el codigo de la clase Datos. Se cambio el nombre de la Clase y atributos. Tambien se elimino la biblioteca requests y lineas de codigo relacionadas dado a que decidi trabajar con los datos de forma local y apegarme al cronograma de los contenidos del curso

--- Tareas/TAREA_CINCO/src/datos.py
+++ Tareas/TAREA_CINCO/src/datos.py
@@ -1,47 +1,42 @@
 import pandas as pd
-import requests
 
-class BTS:
-    def __init__(self, web):
-        self.web = web 
-    
-    # Metodo encargado de  la obtencion de datos de las pagina web
+class Datos:
+    def __init__(self):
+        self.df = None
+        self.columnas = ["Año", "Llegadas puntuales", "A tiempo (%)", "Retrasos en la llegada", "Demorado (%)", "Vuelo cancelado", "Cancelado (%)", "desviado", "Las operaciones de vuelo"]
+      
     def obtener_datos(self):
-        response = requests.get(self.web) 
-        html = response.text
-        
-        # En esta parte implementare un cogido encargado de la extracion de los datos del HTML
-        
-        with open("aerolineas.csv", "w") as f:
-            f.write()
-            f.write()
-            f.write()
-        
-        
-        # Usar iteradores en las transformaciones a columnas
-        
+        try:
+            self.df = pd.read_csv("aerolineas.csv", delimiter=',', header=None, names=self.columnas)
+            print(self.df.head())  
+        except FileNotFoundError:
+            print("El archivo no se encontro.")
+        except Exception as e:
+            print("Ocurrio un error durante la carga de datos:", str(e))
 
-        return  
+        for columna in self.columnas:
+            if self.df[columna].dtype == "object": 
+                self.df[columna] = pd.to_numeric(self.df[columna].str.replace(".", " ").str.replace(",", "."))
+
+        return self.df
+
+    def limpieza_datos(self):             
+        columnas_innecesarias = ["Demorado (%)", "Vuelo cancelado", "Cancelado (%)", "desviado"]
+        self.df.drop(columns=columnas_innecesarias, inplace=True)
+        self.df.dropna(inplace=True)
         
-        
-        
-    def limpieza_datos():
-        
-        # Codigo relacionado con la limpieza de datos. 
-        # Este metodo puede eliminar filas o columnas innecesarias, manejar valores nulos, convertir tipos de datos.
-        
-        return 
+        return self.df
     
-    
-    def analisis_datos():
+    def analisis_datos(self):                
+        descripcion = self.df.describe()
+        def contar_vuelos_por_aerolinea():
+            for aerolinea in self.df["Año"].unique():
+                yield aerolinea, len(self.df[self.df["Año"] == aerolinea])
         
-        # Codigo relacionado con analisis de los datos. (Mediante Pandas)
-        # Este metodo esta encargado de calcular valores, identificar tendencias, encontrar patrones y buscar valores atıpicos.
-        # Usar un generador para crear informes sobre la cantidad de vuelos y otro pasajeros por aerolınea.
-        # Usar un iterador para filtrar datos de diferentes aerolıneas y otro iterador para recorrer el DataFrame y seleccionar solo las filas correspondientes a una aerolınea durante la limpieza.
-
-
-        return 
+        def obtener_detalles_pasajeros_por_aerolinea():
+            for aerolinea in self.df["Año"].unique():
+                detalles = self.df[self.df["Año"] == aerolinea]["Llegadas puntuales"].describe()
+                yield aerolinea, detalles
         
+        return descripcion, contar_vuelos_por_aerolinea(), obtener_detalles_pasajeros_por_aerolinea()
 
-bts = BTS("https://www.transtats.bts.gov/")
