8483ac9790f0e72573ea58c1912a56b2816249e8
emmanuelavilesucr <emmanuel.aviles@ucr.ac.cr>
2024-02-13 15:28:07 -0600

    Agregar logica de la funcion agrupamiento

--- Tareas/TAREA_SEIS/src/cars_analysis/clustering.py
+++ Tareas/TAREA_SEIS/src/cars_analysis/clustering.py
@@ -1,6 +1,9 @@
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
 
-def agrupamiento():
+def agrupamiento(X, n_clusters=4):
+    scaler = StandardScaler()
+    X_normalizado = scaler.fit_transform(X)
+    kmeans = KMeans(n_clusters=n_clusters, random_state=42)
     
-    return
+    return kmeans.fit_predict(X_normalizado)
