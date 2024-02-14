10afa8dcd2f25c0e01da8ea6e744245f3527b36c
emmanuelavilesucr <emmanuel.aviles@ucr.ac.cr>
2024-02-13 19:39:52 -0600

    Agregar comentarios

--- Tareas/TAREA_SEIS/src/cars_analysis/clustering.py
+++ Tareas/TAREA_SEIS/src/cars_analysis/clustering.py
@@ -4,6 +4,12 @@ from sklearn.preprocessing import StandardScaler
 
 
 def agrupamiento(data):
+    """
+    Esta funcion es la encargada de realizar el agrupamiento de datos utilizando el algoritmo K-means.
+
+    :param: DataFrame que contiene los datos.
+    :return: La columna 'cluster' indica la asignacion de grupo para cada punto.
+    """
      
     X = data[['selling_price', 'year', 'km_driven']]
     scaler = StandardScaler()
